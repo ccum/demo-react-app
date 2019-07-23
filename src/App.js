4c630f566efc5398684012a1d10c2ef6bbab6975
Cesar Cueva <cesar.cueva@bbva.com>
2019-07-22 23:36:00 -0500

    mostrando contenido en un componente

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, {Fragment} from 'react';
 import Header from './components/Header'
 import Footer from './components/Footer'
+import ListaProductos from './components/ListaProductos'
 
 function App() {
   const fecha = new Date().getFullYear();
   return (
       <Fragment>
         <Header titulo='Tienda virtual' />
-      
+        <ListaProductos/>
         <Footer
           fecha={fecha}
         />
---
+++ src/components/ListaProductos.js
@@ -0,0 +1,31 @@
+import React, { Component, Fragment } from 'react';
+import Producto from './Producto'
+
+class ListaProductos extends Component {
+    state = {
+        productos:[
+        {id: 1, name : 'Camisa React', precio: 30},
+        {id: 2, name : 'Camisa Vuejs', precio: 30},
+        {id: 3, name : 'Camisa Angular', precio: 30},
+        {id: 4, name : 'Camisa Node', precio: 30},
+        ],
+        totalCarrito: 500,
+        cliente: 'Cesar Cueva'
+    }
+    render() { 
+        const {productos} = this.state;
+        return (
+            <Fragment>
+                <h1>Lista de productos</h1>
+                {productos.map(producto =>(
+                    <Producto
+                    key = {producto.id}    
+                    producto = {producto}
+                    />
+                ))}
+            </Fragment>
+        );
+    }
+}
+ 
+export default ListaProductos;
